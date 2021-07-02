dc02e3d1f7c30b1b329b8ecb736f4bcd041b0aee
Adam ? <adamznow@gmail.com>
2021-07-02 02:10:56 -0700

    Initial Commit

---
+++ SNKRX-Save-Parser/SNKRX-Save-Parser.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SNKRX_Save_Parser.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
